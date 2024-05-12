dc8bd8da3ed8be612140459cd5e2b6d0b32e9609
ejwoods <ewoods08@gmail.com>
2024-05-11 20:29:49 -0400

    convert App and Home components from inline template to external

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,16 +4,7 @@ import { HomeComponent } from './home/home.component';
 @Component({
   standalone: true,
   selector: 'app-root',
-  template: `
-    <main>
-      <header class="brand-name">
-        <img class="brand-logo" src="/assets/logo.svg"
-        alt="logo" aria-hidden="true">
-      </header>
-      <section class="content">
-        <app-home></app-home>
-      </section>
-    </main>`,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   imports: [HomeComponent]
 })
